a2c0530f2776c4bb7cf830d4403021000dbd8448
UmbraServus <ksprague0592@gmail.com>
2024-07-13 00:04:27 -0500

    with open to create a file with the right format of id.csv, using the data from the rest api, exported it into a csv file.

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import requests
+import sys
+import csv
+"""api module to gather data from a given api."""
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: 1-export_to_CSV.py <employee_id>)")
+        sys.exit(1)
+    
+    else:
+        employee_id = sys.argv[1]
+
+        # Define Base URL/API endpoints
+        base_url = 'https://jsonplaceholder.typicode.com/'
+        employee_url = f'{base_url}/users/{employee_id}'
+        todos_url = f'{base_url}/todos?userId={employee_id}'
+
+        # gets data from api and id from argv[1]
+        employee_data = requests.get(employee_url).json()
+        employee_name = employee_data['name']
+        todo_data = requests.get(todos_url).json()
+        completed_tasks = [task for task in  todo_data if task['completed']]
+
+        # Setup CSV file
+        with open(f'{employee_id}.csv', 'w') as f:
+
+        # write to file
+            for task in completed_tasks:
+                csv.writer(f).writerow([employee_id,
+                                        employee_name,
+                                        task['completed'],
+                                        task['title']
+                                        ])
