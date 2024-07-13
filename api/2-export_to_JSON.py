d70311ddae128885c06146e956b8010d1204004d
UmbraServus <ksprague0592@gmail.com>
2024-07-13 00:53:17 -0500

    using what I learned on CSV export did a specific employee export to json and an export all employees to json.

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import requests
+import sys
+import json
+"""api module to gather data from a given api."""
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: 1-export_to_CSV.py <employee_id>)")
+        sys.exit(1)
+    
+    else:
+        employee_id = sys.argv[1]
+
+        # Define Base URL/API endpoints
+        base_url = 'https://jsonplaceholder.typicode.com/'
+        employee_url = f'{base_url}/users/{employee_id}'
+        todos_url = f'{base_url}/todos?userId={employee_id}'
+
+        # gets data from api and id from argv[1]
+        employee_data = requests.get(employee_url).json()
+        employee_name = employee_data['username']
+        todo_data = requests.get(todos_url).json()
+
+        # setup data to use in writing file
+        tasks = []
+        for task in todo_data:
+            tasks.append({
+                'task': task['title'],
+                'completed': task['completed'],
+                'username': employee_name
+            })
+        
+        # json data
+        json_data = {employee_id: tasks}
+
+        # Setup JSON file
+        with open(f'{employee_id}.JSON', 'w') as f:
+
+        # write to file
+            json.dump(json_data, f)
---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import requests
+import sys
+import json
+"""api module to gather data from a given api."""
+
+if __name__ == "__main__":
+
+
+    # Define Base URL/API endpoints
+    base_url = 'https://jsonplaceholder.typicode.com'
+    employee_url = f'{base_url}/users'
+    todos_url = f'{base_url}/todos'
+
+    # gets employee data from api
+    employee_data = requests.get(employee_url).json()
+
+    json_data = {}
+
+    #get tasks for each employee
+    for employee in employee_data:
+        employee_id = employee['id']
+        employee_name = employee['username']
+
+        # gets tasks data
+        todo_data = requests.get(f'{todos_url}?userId={employee_id}').json()
+
+        # setup data to use in writing file
+        tasks = []
+        for task in todo_data:
+            tasks.append({
+                'task': task['title'],
+                'completed': task['completed'],
+                'username': employee_name
+            })
+        
+        # json data
+        json_data[employee_id] = tasks
+
+    # Setup JSON file
+    with open(f'todo_all_employees.JSON', 'w') as f:
+
+    # write to file
+        json.dump(json_data, f)
