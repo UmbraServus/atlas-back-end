2bffdc71a3edeeff01c4b8dd8ef8b95c51488c24
UmbraServus <ksprague0592@gmail.com>
2024-07-17 10:43:52 -0500

    oeo8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -26,9 +26,10 @@ def get_employee_todo_info(employee_id):
     for task in completed_tasks:
         print(f"\t {task['title']}")
 
+
 if __name__ == "__main__":
     """ beginning of main to use to get employee info """
-    
+
     if len(sys.argv) != 2:
         print("Usage: python script.py <employee_id>)")
         sys.exit(1)
